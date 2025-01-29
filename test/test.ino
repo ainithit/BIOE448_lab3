58887ae8d801e365c8538bce49c750f2a222e691
ainithit <nithitbc@gmail.com>
2025-01-29 14:43:31 -0600

    LCD Printing

--- test/test.ino
+++ test/test.ino
@@ -3,9 +3,13 @@
 void setup() {
   // put your setup code here, to run once:
   LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+  lcd.begin(16, 2);
+  lcd.print("My name is: ");
+  lcd.setCursor(0, 1);
+  lcd.print("Ai");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  delay(50);
 }
